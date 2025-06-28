15bca1a788e078f596dfcc4d48316505cfd14015
Dilith Samarakoon <e23336@eng.pdn.ac.lk>
2025-06-28 09:55:54 +0530

    Update canvas.h

--- include/canvas.h
+++ include/canvas.h
@@ -1,28 +1,23 @@
 #ifndef CANVAS_H
 #define CANVAS_H
 
-#include <stddef.h> // for size_t
+#include <stddef.h> // for size_t (unsigned long long)
 
-// Define the canvas structure
+// Declare the canvas structure
 typedef struct {
     size_t width;
     size_t height;
     float** pixels;  // 2D array: pixels[y][x]
 } canvas_t;
 
-typedef struct {
-    float thickness;
-    float intensity;
-} brush_t;
-
 // Create and destroy canvas
-canvas_t* canvas_create(size_t width, size_t height);
+canvas_t *canvas_create(size_t width, size_t height);
 void canvas_destroy(canvas_t* canvas);
 
-// Draw a floating-point line with thickness using DDA
-void draw_line_f(canvas_t* canvas, float x0, float y0, float x1, float y1, const brush_t* brush);
-
 // Set a pixel with floating-point coordinates and intensity
-void set_pixel_f(canvas_t* canvas, float x, float y, const brush_t* brush);
+void set_pixel_f(canvas_t* canvas, float x, float y, float intensity);
+
+// Draw a floating-point line with thickness using DDA
+void draw_line_f(canvas_t* canvas, float x0, float y0, float x1, float y1, float thickness);
 
 #endif // CANVAS_H

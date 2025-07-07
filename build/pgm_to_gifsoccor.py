aeaf520f3b99bb21628dfe40d0e8f43c440976c3
Dilith Samarakoon <e23336@eng.pdn.ac.lk>
2025-07-07 12:12:21 +0530

    Delete build/pgm_to_gifsoccor.py

--- build/pgm_to_gifsoccor.py
+++
@@ -1,8 +0,0 @@
-import imageio.v2 as imageio
-import glob
-
-filenames = sorted(glob.glob("frames/frame_*.pgm"))
-with imageio.get_writer("frames/rotation.gif", mode="I", fps=10) as writer:
-    for file in filenames:
-        img = imageio.imread(file)
-        writer.append_data(img)
